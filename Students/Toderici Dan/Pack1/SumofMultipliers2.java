455de298af34a34c1b16462415f7cc6aef7e45b1
todericidan <toderici_dan@yahoo,com>
2015-07-10 12:28:23 +0300

    assigment pack1

--- Students/Toderici Dan/Pack1/SumofMultipliers2.java
+++ Students/Toderici Dan/Pack1/SumofMultipliers2.java
@@ -19,27 +19,21 @@ public final class SumofMultipliers {
 		 * java.io.*
 		 */
 		System.out.println("Insert number:");
-		int number, sumofMultiplier = 0;
+		int number;
+		int sumofMultiplier3,sumofMultiplier5,sumofMultiplier15,sumofMultiplier = 0;
 		number = numberFromKeyboard.nextInt();
 		/*
 		 * solving twist 2 for 2,147,483,648 a problem will occur if the number
 		 * var is int for 9,223,372,036,854,775,808 a problem will occur if the
 		 * number var is long
 		 */
-		for (int i = 3; i <= number; i += 3) {
-			sumofMultiplier += i;
-		}
-		for (int i = 5; i <= number; i += 5) {
-			sumofMultiplier += i;
-		}
-		for (int i = 15; i <= number; i += 15) {
-			sumofMultiplier -= i;
-		}
-
+		sumofMultiplier3 =((number * (number + 1)) / 2) *3;
+		sumofMultiplier5 =((number * (number + 1)) / 2) *5;
+		sumofMultiplier15 =((number * (number + 1)) / 2) *15;
+		sumofMultiplier= sumofMultiplier3 + sumofMultiplier5 - sumofMultiplier15;
 		System.out.println("SumofMultipliers of 3 or 5 smaller than " + number);
 		System.out.println(sumofMultiplier);
 		
 		numberFromKeyboard.close();
 	}
-
 }
