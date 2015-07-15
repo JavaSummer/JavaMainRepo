6f7d460432a7e5408c37c5f759f1b5404b88d077
Kovacs Gellert <kovacsgellertpal@gmail.com>
2015-07-15 21:03:37 +0300

    Update SumOfMultiples to include efficient method.

--- Students/Kovacs Gellert/Assignment1/SumOfMultiples.java
+++ Students/Kovacs Gellert/Assignment1/SumOfMultiples.java
@@ -9,12 +9,34 @@ public class SumOfMultiples {
 		int n = in.nextInt();
 		in.close();
 		
+		int sum = inefficientSum(n);
+		System.out.println("The sum is [O(n) time algorithm]: " + sum + ".");
+		
+		sum = efficientSum(n);
+		System.out.println("The sum is [constant time algorithm]: " + sum + ".");
+	}
+	
+	//O(n) time algorithm
+	public static int inefficientSum(int n) {
 		int sum = 0;
 		for (int i = 0; i < n; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
 				sum+=i;
 			}
 		}
-		System.out.println("The sum is " + sum + ".");
+		return sum; 
+	}
+	
+	//constant time algorithm
+	public static int efficientSum(int n) {
+		int nThree = n / 3;
+		int nFive = n/ 5;
+		int nFifteen = n /15;
+		
+		int threeSum = 3*(nThree*(nThree + 1))/2;
+		int fiveSum = 5*(nFive*(nFive+1))/2;
+		int fifteenSum = 15*(nFifteen*(nFifteen+1))/2;
+		
+		return threeSum + fiveSum - fifteenSum;
 	}
 }
