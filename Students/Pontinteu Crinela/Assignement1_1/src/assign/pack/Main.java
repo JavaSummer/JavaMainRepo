64053aac08e7216104b2eeffe9c00a76b4d24041
crinela <potinteu.crinela@yahoo.com>
2015-07-18 13:05:36 +0300

    Final modification

--- Students/Pontinteu Crinela/Assignement1_1/src/assign/pack/Main.java
+++ Students/Pontinteu Crinela/Assignement1_1/src/assign/pack/Main.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-
+		// TODO Auto-generated method stub
+		
 		int sum, givenNr;
 		int limitSum3, limitSum5, limitSum15;
 
