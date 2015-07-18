b479b0edfaa910866af47cb092fd58811b2d3878
crinela <potinteu.crinela@yahoo.com>
2015-07-18 11:59:56 +0300

    A modification of the first assignment

--- Students/Pontinteu Crinela/Assignement1_1/src/assign/pack/Main.java
+++ Students/Pontinteu Crinela/Assignement1_1/src/assign/pack/Main.java
@@ -6,27 +6,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int sum = 0;
-		int givenNr;
+
+		int sum,givenNr;
 
 		Scanner in = new Scanner(System.in);
 		System.out.println("Input the number ");
 		givenNr = in.nextInt();
 
-		int endLimit;
-		endLimit = (givenNr - 1) / 3;
+		int limitSum3, limitSum5, limitSum15;
+		
+		if ((givenNr % 3) == 0) {
+			limitSum3 = givenNr / 3 - 1;
+		} else {
+			limitSum3 = givenNr / 3;
+		}
+		
+		if ((givenNr % 5) == 0) {
+			limitSum5 = givenNr / 5 - 1;
+		} else {
+			limitSum5 = givenNr / 5;
+		}
+		
+		if ((givenNr % 15) == 0) {
+			limitSum15 = givenNr / 15 - 1;
+		} else {
+			limitSum15 = givenNr / 15;
+		}
 
-		for (int i = 1; i <= endLimit; i++) {
-			sum += i * 3;
-		}
-		endLimit = (givenNr - 1) / 5;
-		for (int i = 1; i <= endLimit; i++) {
-			sum += i * 5;
-		}
-		System.out.print("The sum of all the mupliples of 3 and 5 below ");
-		System.out.print(givenNr);
-		System.out.print(" is: " + sum);
+		sum =  3* ((limitSum3*(limitSum3+1))/2);
+		sum += 5* ((limitSum5*(limitSum5+1))/2);
+		sum -= 15* ((limitSum15*(limitSum15+1))/2);
+	
+		System.out.print("The sum of all the mupliples of 3 and 5 below "+ givenNr +" is: " + sum);
+
 	}
 
 }
