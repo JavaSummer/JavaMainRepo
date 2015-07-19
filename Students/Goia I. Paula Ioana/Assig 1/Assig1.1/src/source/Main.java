4a7f3e6fc3a0059996eacc9f4fdef606dde7ddea
Goia I.PaulaIoana <goiapaulaioana@gmail.com>
2015-07-19 15:36:29 +0300

    OOP # 1 version 2 :D

--- Students/Goia I. Paula Ioana/Assig 1/Assig1.1/src/source/Main.java
+++ Students/Goia I. Paula Ioana/Assig 1/Assig1.1/src/source/Main.java
@@ -8,12 +8,16 @@ public class Main {
 		Scanner n = new Scanner(System.in);
 		int nr = n.nextInt();
 		long s = 0;
-		for(int i = 1; i < nr ; i++){
-			if(i % 3==0 || i%5==0){
-				s += i;
-			}
-		}
+		int s1 = 0;
+		int s2 = 0;
+		int s3 = 0;
+		s1 = (3*((nr-1)/3)*((nr-1)/3+1))/2;
+		s2 = (5*((nr-1)/5)*((nr-1)/5+1))/2;
+		s3 = (15*((nr-1)/15)*((nr-1)/15+1))/2;
+		s=s1+s2-s3;
 		System.out.println("The sum is " + s);
+		
+		
 	}
 
 }
