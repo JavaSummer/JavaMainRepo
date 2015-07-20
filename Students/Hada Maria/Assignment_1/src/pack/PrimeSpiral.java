4d2b0e6546d59bfc1c66c138f05a5b97633da839
mariahada <mhada02@gmail.com>
2015-07-20 22:15:57 +0300

    Assignment 1 update 3.

--- Students/Hada Maria/Assignment_1/src/pack/PrimeSpiral.java
+++ Students/Hada Maria/Assignment_1/src/pack/PrimeSpiral.java
@@ -16,26 +16,51 @@ public class PrimeSpiral {
 	 * 
 	 * @param n
 	 *            - calculating the matrix diagonals
-	 * @return - spiral matrix
+	 * @return - ratio
 	 */
-	public static int[][] buildSpiralMatrix(final int n) {
+	public static void primeRatio(final int n) {
 
-		int[][] spiralMatrix = new int[n][n];
+		final int below = 10;
+		final int cent = 100;
+		double ratio = 0;
 		int sideLength = n;
-		int m = n;
-		int i = 0;
+		int ratioPrime = 0;
+		int diagonalNumbers = 13;
+		int primeNumbers = 8;
+		int botRightCorner = 49, botLeftCorner = 43;
+		int topLeftCorner = 37, topRightCorner = 31;
+		int k = 6;
 
+		ratio = ((double) (primeNumbers) / (double) (diagonalNumbers)) * cent;
+		ratioPrime = (int) (ratio);
+		sideLength = sideLength + 2;
+		k = k + 2;
 		do {
-			spiralMatrix[m - 1][m - 1] = sideLength * sideLength;
-			spiralMatrix[m - 1][i] = spiralMatrix[m - 1][m - 1] - sideLength + 1;
-			spiralMatrix[i][i] = spiralMatrix[m - 1][i] - sideLength + 1;
-			spiralMatrix[i][m - 1] = spiralMatrix[i][i] - sideLength + 1;
-			i++;
-			m = m - 1;
-			sideLength = sideLength - 2;
-		} while (sideLength >= 1);
+			botRightCorner = sideLength * sideLength;
+			if (checkIfPrime(botRightCorner) == 1) {
+				primeNumbers++;
+			}
+			botLeftCorner = botRightCorner - k;
+			if (checkIfPrime(botLeftCorner) == 1) {
+				primeNumbers++;
+			}
+			topLeftCorner = botLeftCorner - k;
+			if (checkIfPrime(topLeftCorner) == 1) {
+				primeNumbers++;
+			}
+			topRightCorner = topLeftCorner - k;
+			if (checkIfPrime(topRightCorner) == 1) {
+				primeNumbers++;
+			}
+			k = k + 2;
+			sideLength = sideLength + 2;
+			diagonalNumbers = diagonalNumbers + 4;
+			ratio = ((double) (primeNumbers) / (double) (diagonalNumbers)) * cent;
+			ratioPrime = (int) (ratio);
 
-		return spiralMatrix;
+		} while (ratioPrime >= below);
+		System.out.print("The side length is " + sideLength + " ");
+		System.out.println("and the ratio is " + ratioPrime + "%.");
 	}
 
 	/**
@@ -44,51 +69,22 @@ public class PrimeSpiral {
 	 *            -> to be checked
 	 * @return -> 1 if prime, 0 otherwise
 	 */
-	public static int checkIfPrime(final int number) {
+	public static int checkIfPrime(int number) {
 
-		int prime = 0;
-		for (int i = 2; i < number; i++) {
+		int prime = 1;
+		if (number % 2 == 0) {
+			prime = 0;
+		}
+		for (int i = 3; i * i <= number; i += 2) {
 			if (number % i == 0) {
-				prime = 1;
+				prime = 0;
 			}
 		}
-		if (prime == 0) {
+		if (prime == 1) {
 			return 1;
 		} else {
 			return 0;
 		}
-	}
-
-	/**
-	 * 
-	 * @param spiralM
-	 *            ->the spiral matrix
-	 * @param n
-	 *            -> size of the matrix
-	 * @return ratioPrime
-	 */
-	public static int primeRatio(final int[][] spiralM, final int n) {
-
-		double ratio = 0;
-		int ratioPrime = 0;
-		int diagonalNumbers = 2 * n - 1;
-		int primeNumbers = 0;
-		final int cent = 100;
-
-		for (int row = 0; row < n; row++) {
-			for (int col = 0; col < n; col++) {
-				if ((row == col) || (row + col == n - 1)) {
-					if (spiralM[row][col] != 1) {
-						if (checkIfPrime(spiralM[row][col]) == 1) {
-							primeNumbers++;
-						}
-					}
-				}
-			}
-		}
-		ratio = ((double) (primeNumbers) / (double) (diagonalNumbers)) * cent;
-		ratioPrime = (int) (ratio);
-		return ratioPrime;
 	}
 
 	/**
@@ -98,33 +94,7 @@ public class PrimeSpiral {
 	 */
 	public static void main(final String[] args) {
 
-		int[][] spiralMatrix = new int[1][1];
-		final int below = 10;
 		final int start = 7;
-		int sideLength = start;
-		int ratio = 0;
-
-		spiralMatrix = buildSpiralMatrix(sideLength);
-		ratio = primeRatio(spiralMatrix, sideLength);
-		/*for (int i = 0; i < sideLength; i++) {
-			for (int j = 0; j < sideLength; j++) {
-				System.out.print(spiralMatrix[i][j] + "\t");
-			}
-			System.out.println();
-		}*/
-		System.out.print("The side length is " + sideLength + " ");
-		System.out.println(ratio + "%");
-
-		do {
-			sideLength = sideLength + 2;
-			spiralMatrix = buildSpiralMatrix(sideLength);
-			ratio = primeRatio(spiralMatrix, sideLength);
-			System.out.print("The side length is " + sideLength + " ");
-			System.out.println(ratio + "%");
-		} while (ratio >= below);
-
-		System.out.println();
-		System.out.print("The side length is " + sideLength + " ");
-		System.out.print("and the ratio is " + ratio + "%");
+		primeRatio(start);
 	}
 }
