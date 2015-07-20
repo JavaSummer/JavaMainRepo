158d4a5dbbddd488f424a3c90aa0e2708c73e5aa
Fejer Alpar <fejeralpar@yahoo.com>
2015-07-20 12:43:26 +0300

    I know it's late, but i made a mistake here.

--- Students/Fejer K. Alpar/W1/assignment1.1/src/sumultipleTwist2.java
+++ Students/Fejer K. Alpar/W1/assignment1.1/src/sumultipleTwist2.java
@@ -8,20 +8,22 @@ public class sumultipleTwist2 {
 		sum2 = 5 * ((a / 5 - 1) * (a / 5)) / 2;
 		sum3 = 15 * ((a / 15) * (a / 15 + 1)) / 2;
 		while (b == 0) {
-				if (a % 3 == 0)
-					sum1 += a;
-				else if (a % 5 == 0)
-					sum2 += a;
-				if ((sum1 + sum2 > Integer.MAX_VALUE) && (c == 0)) {
-					System.out.println("Int overflow for " + a);
-					c++;
+			if ((a % 3 == 0) && (a % 5 == 0))
+				sum3 += a;
+			else if (a % 3 == 0)
+				sum1 += a;
+			else if (a % 5 == 0)
+				sum2 += a;
+			if ((sum1 + sum2 > Integer.MAX_VALUE) && (c == 0)) {
+				System.out.println("Int overflow for " + a);
+				c++;
 
-				}
-				if (Long.MAX_VALUE - sum1 < sum2 - sum3) {
+			}
+			if (Long.MAX_VALUE - sum1 < sum2 - sum3) {
 
-					System.out.println("Long overflow for " + a);
-					b++;
-				}
+				System.out.println("Long overflow for " + a);
+				b++;
+			}
 			a++;
 		}
 	}
