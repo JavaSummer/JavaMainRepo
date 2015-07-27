7ba4b0e431f782b8cd1fb43ae567cc61de389590
Negrau Adina <negrau.adina@gmail.com>
2015-07-27 13:40:09 +0300

    I did not manage to find out why I receive the error: "Error: Could not find or load main class partI.InputOutput" when I try to run the initial project with the required modifications, so I've created a new one (with the same code) which, in a bizarre way, seems to work just fine.

---
+++ Students/Negrău Adina/AssignmentII/src/partI/InputOutput.java
@@ -0,0 +1,11 @@
+package partI;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+		InputOutputFunctions f = new InputOutputFunctions();
+		f.openFile();
+		f.readNextCommand();
+		
+	}
+}
---
+++ Students/Negrău Adina/AssignmentII/src/partI/InputOutputFunctions.java
@@ -0,0 +1,108 @@
+package partI;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import partI.PolynomialFunctions;
+
+public class InputOutputFunctions {
+	public PolynomialFunctions polFct = new PolynomialFunctions();
+	private Scanner x;	
+	/*
+	 * Open a file - "pol.txt" - for reading
+	 */
+	public void openFile()
+	{
+		try{
+			x = new Scanner(new File("pol.txt"));
+			
+		}
+		catch(Exception e){
+			System.out.println("Could not open the file");
+		}
+	}
+
+	/*
+	 * Open a file for writing
+	 * s - String (what to write)
+	 * filename - String (where to write)
+	 */
+	public void writeToFile(String s, String filename){
+		try{
+			FileWriter fw = new FileWriter(filename, true);
+			fw.write(s);
+			fw.write("\n");
+			fw.close();
+		}
+		catch(IOException e){
+			System.err.println("Could not write to file");
+		}
+	}
+	/*
+	 * Create a polynomial by reading the information from a line
+	 */
+	public Polynomial createPol(){
+		String currentLine = x.nextLine();
+		int j=0;
+		String[] arr = currentLine.split(" ");
+		int[] v = new int[arr.length];
+		if(arr.length!=0){
+			for(int k=arr.length-1; k>=0; k--){
+				v[j] = Integer.parseInt(arr[k]);
+				j++;
+			}
+		}
+		Polynomial p = new Polynomial(arr.length-1, v);
+		return p;
+	}
+	
+	/*
+	 * Read the next k commands from the file line by line
+	 */
+	public void readNextCommand(){
+		Polynomial p = createPol();
+		Polynomial p2 = createPol();
+		InputOutputFunctions IOF = new InputOutputFunctions();
+		while(x.hasNext()){
+			String currentLine = x.nextLine();
+			if(currentLine.equals("ADD")){
+				Polynomial result  = polFct.add(p, p2);
+				IOF.writeToFile("The result of addition is: " + result.toString(), "output.txt");				
+			}
+			else if(currentLine.equals("DELETE")){
+				Polynomial result = polFct.delete(p, p2);
+				IOF.writeToFile("The result of delete is: " + result.toString(), "output.txt");		
+
+			}
+			else if(currentLine.equals("MULTIPLY")){
+				Polynomial result = polFct.multiply(p, p2);
+				IOF.writeToFile("The result of multiplication is: " + result.toString(), "output.txt");
+
+			}
+			else{
+				String[] words = currentLine.split(" ");
+				if(words[0].equals("MUL_SCAL")){
+					int value = Integer.parseInt(words[1]);
+					Polynomial result1 = polFct.multiplyWith(p, value);
+					IOF.writeToFile("The result of multiplication with " + value + " is: ", "output.txt");
+					IOF.writeToFile(result1.toString(), "output.txt");
+					Polynomial result2 =  polFct.multiplyWith(p2, value);
+					IOF.writeToFile(result2.toString(), "output.txt");
+
+					
+				}
+				else if(words[0].equals("EVAL")){
+					int value = Integer.parseInt(words[1]);
+					IOF.writeToFile("The result of evaluation on " + value + " is: ","output.txt");
+					Polynomial result1 = polFct.evalOn(p, value);
+					IOF.writeToFile(result1.toString(), "output.txt");
+					Polynomial result2 = polFct.evalOn(p2, value);	
+					IOF.writeToFile(result2.toString(), "output.txt");
+				}
+			}
+		}
+	}
+	
+
+}
