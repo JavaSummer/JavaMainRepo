8fde96a61075c131d9278df5a40f598ebe81e284
crinela <potinteu.crinela@yahoo.com>
2015-07-30 21:55:43 +0300

    Assignment2_2

---
+++ Students/Pontinteu Crinela/Assignment2_2/src/assig/matrix/Main.java
@@ -0,0 +1,103 @@
+package assig.matrix;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner diskScanner = new Scanner(new File("matrix.txt"));
+		BigDecimal a[][];
+		BigDecimal b[][];
+		System.out.println("The matrix a is:");
+		a = createMatrix(diskScanner);
+		printMatrix(a);
+		diskScanner.nextLine();
+		diskScanner.nextLine();
+		System.out.println();
+		System.out.println("The matrix b is: ");
+		b = createMatrix(diskScanner);
+		printMatrix(b);
+		System.out.println();
+
+		BigDecimal c[][];
+
+		System.out.println("a+b: ");
+		c = MatrixOperations.add(a, b);
+		if (c != null) {
+			printMatrix(c);
+		}
+		System.out.println();
+
+		System.out.println("a-b: ");
+		c = MatrixOperations.subtract(a, b);
+		if (c != null) {
+			printMatrix(c);
+		}
+		System.out.println();
+
+		System.out.println("a*b: ");
+		c = MatrixOperations.multiply(a, b);
+		if (c != null) {
+			printMatrix(c);
+		}
+		System.out.println();
+
+		diskScanner.nextLine();
+		BigDecimal d = new BigDecimal("0");
+		d = diskScanner.nextBigDecimal();
+		System.out.println("a*" + d + ":");
+		c = MatrixOperations.multiplyScalar(a, d);
+		printMatrix(c);
+		System.out.println();
+
+		System.out.println("The determinant of a: ");
+		d = MatrixOperations.determinant(a);
+		String str = d.toPlainString();
+		System.out.println(str);
+		System.out.println();
+
+		System.out.print("The matrices a and b ");
+		System.out.println(MatrixOperations.areEqual(a, b) ? "ARE equal" : "ARE NOT equal");
+		System.out.println();
+
+		System.out.print("The matrix a ");
+		System.out.println(MatrixOperations.isZeroMatrix(a) ? "IS the zero matrix" : "IS NOT the zero matrix");
+		System.out.println();
+		
+		System.out.print("The matrix a ");
+		System.out.println(MatrixOperations.isIdentityMatrix(a) ? "IS the identity matrix" : "IS NOT the identity matrix");
+		System.out.println();
+		
+		System.out.print("The fill degree of the matrix a is:");
+		d = MatrixOperations.fillDegree(a);
+		str = d.toPlainString();
+		System.out.println(str);
+
+	}
+
+	public static BigDecimal[][] createMatrix(Scanner diskScanner) {
+		int rows = diskScanner.nextInt();
+		int cols = diskScanner.nextInt();
+		BigDecimal a[][] = new BigDecimal[rows][cols];
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				a[i][j] = diskScanner.nextBigDecimal();
+		return a;
+	}
+
+	public static void printMatrix(BigDecimal[][] a) {
+		int rows = a.length;
+		int cols = a[0].length;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				String str = a[i][j].toPlainString();
+				System.out.print(str + " ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
---
+++ Students/Pontinteu Crinela/Assignment2_2/src/assig/matrix/MatrixOperations.java
@@ -0,0 +1,179 @@
+package assig.matrix;
+
+import java.math.BigDecimal;
+
+public class MatrixOperations {
+	public MatrixOperations() {
+
+	}
+
+	public static BigDecimal[][] add(BigDecimal[][] a, BigDecimal[][] b) {
+		BigDecimal c[][];
+		int rowsA = a.length;
+		int colsA = a[0].length;
+		int rowsB = b.length;
+		int colsB = b[0].length;
+
+		if ((rowsA == rowsB) && (colsA == colsB)) {
+			c = new BigDecimal[rowsA][colsA];
+			for (int i = 0; i < rowsA; i++)
+				for (int j = 0; j < colsA; j++)
+					c[i][j] = a[i][j].add(b[i][j]);
+		} else {
+			System.out.println("The matrices can not be added !");
+			return null;
+		}
+		return c;
+	}
+
+	public static BigDecimal[][] subtract(BigDecimal[][] a, BigDecimal[][] b) {
+		BigDecimal c[][];
+		int rowsA = a.length;
+		int colsA = a[0].length;
+		int rowsB = b.length;
+		int colsB = b[0].length;
+
+		if ((rowsA == rowsB) && (colsA == colsB)) {
+			c = new BigDecimal[rowsA][colsA];
+			for (int i = 0; i < rowsA; i++)
+				for (int j = 0; j < colsA; j++)
+					c[i][j] = a[i][j].subtract(b[i][j]);
+		} else {
+			System.out.println("The matrices can not be subtracted !");
+			return null;
+		}
+		return c;
+	}
+
+	public static BigDecimal[][] multiply(BigDecimal[][] a, BigDecimal[][] b) {
+		BigDecimal c[][];
+		int rowsA = a.length;
+		int colsA = a[0].length;
+		int rowsB = b.length;
+		int colsB = b[0].length;
+
+		if (colsA == rowsB) {
+			BigDecimal temp = new BigDecimal(0.00);
+			c = new BigDecimal[rowsA][colsB];
+			for (int i = 0; i < rowsA; i++) {
+				for (int j = 0; j < colsB; j++) {
+					for (int k = 0; k < rowsB; k++) {
+						temp = temp.add(a[i][k].multiply(b[k][j]));
+					}
+					c[i][j] = temp;
+					temp = temp.subtract(temp);
+				}
+			}
+		} else {
+			System.out.println("The matrices can not be multiplied !");
+			return null;
+		}
+		return c;
+	}
+
+	public static BigDecimal[][] multiplyScalar(BigDecimal[][] a, BigDecimal d) {
+		BigDecimal c[][];
+		int rowsA = a.length;
+		int colsA = a[0].length;
+		c = new BigDecimal[rowsA][colsA];
+		for (int i = 0; i < rowsA; i++)
+			for (int j = 0; j < colsA; j++) {
+				c[i][j] = d.multiply(a[i][j]);
+			}
+		return c;
+	}
+	
+	public static boolean areEqual(BigDecimal[][] a, BigDecimal[][] b) {
+		if ((a.length == b.length) && (a[0].length == b[0].length)) {
+			for (int i=0; i<a.length; i++)
+				for (int j=0; j<a[0].length; j++)
+					if (!a[i][j].equals(b[i][j]))
+						return false;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public static boolean isZeroMatrix(BigDecimal[][] a) {
+		BigDecimal zero = new BigDecimal ("0");
+			for (int i=0; i<a.length; i++)
+				for (int j=0; j<a[0].length; j++)
+					if (a[i][j].compareTo(zero) != 0)
+						return false;
+			return true;
+	}
+	
+	public static boolean isIdentityMatrix(BigDecimal[][] a) {
+		BigDecimal zero = new BigDecimal ("0");
+		BigDecimal one= new BigDecimal ("1");
+			for (int i=0; i<a.length; i++)
+				for (int j=0; j<a[0].length; j++)
+					if (i == j){
+						if (a[i][j].compareTo(one) != 0)
+							return false;
+					}else
+					if (a[i][j].compareTo(zero) != 0)
+						return false;
+			return true;
+	}
+	
+	public static BigDecimal fillDegree(BigDecimal[][] a) {
+		BigDecimal zero = new BigDecimal ("0");
+		int count = 0;
+		for (int i=0; i<a.length; i++)
+			for (int j=0; j<a[0].length; j++)
+				if (a[i][j].compareTo(zero) == 0)
+					count++;
+		BigDecimal totalNr = new BigDecimal ((a.length-1)*(a[0].length-1));
+		BigDecimal zeroNr = new BigDecimal (count);
+		if (count == 0) {
+			BigDecimal one= new BigDecimal ("1");
+			return one;
+		}		
+		return zeroNr.divide(totalNr);
+	}
+
+	public static BigDecimal determinant(BigDecimal[][] a) {
+		BigDecimal det = new BigDecimal("0.00");
+		if (a.length == a[0].length)
+			det = determinantRecursive(a);
+		else {
+			System.out.println("The determinant can not be calculated !");
+			return null;
+		}
+		return det;
+	}
+
+	public static BigDecimal determinantRecursive(BigDecimal[][] mat) {
+		BigDecimal sum = new BigDecimal("0.00");
+		if (mat.length == 1) {
+			return (mat[0][0]);
+		}
+		if (mat.length == 2) {
+			return (mat[0][0].multiply(mat[1][1]).subtract(mat[1][0].multiply(mat[0][1])));
+		}
+		for (int i = 0; i < mat.length; i++) { // finds determinant using
+												// row-by-row expansion
+			BigDecimal[][] smallerMat = new BigDecimal[mat.length - 1][mat.length - 1];
+			// creates the smaller matrix from the expansion
+			for (int a = 1; a < mat.length; a++) {
+				for (int b = 0; b < mat.length; b++) {
+					if (b < i) {
+						smallerMat[a - 1][b] = mat[a][b];
+					} else if (b > i) {
+						smallerMat[a - 1][b - 1] = mat[a][b];
+					}
+				}
+			}
+
+			if (i % 2 == 0) { // sign changes based on i
+				sum = sum.add(mat[0][i].multiply(determinantRecursive(smallerMat)));
+			} else {
+				sum = sum.add((mat[0][i].multiply(determinantRecursive(smallerMat))).negate());
+			}
+		}
+		return (sum); // returns determinant value. once stack is finished,
+						// returns final determinant.
+	}
+}
