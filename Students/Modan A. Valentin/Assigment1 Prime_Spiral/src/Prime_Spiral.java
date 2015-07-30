5ca70d2610688a4f4bbf0732677b454cd0923b66
vlcmodan <valentinmodan@yahoo.com>
2015-07-30 15:10:12 +0300

    Prime spiral 2.0

--- Students/Modan A. Valentin/Assigment1 Prime_Spiral/src/Prime_Spiral.java
+++ Students/Modan A. Valentin/Assigment1 Prime_Spiral/src/Prime_Spiral.java
@@ -3,38 +3,49 @@ public class Prime_Spiral {
 
 	public static void main(String[] args)
 	{
-		int mare=10000;
-		int a[]=new int [100000];
-		int i,k;
-		for(i=3;i<=mare;i++)
-			if(i%2==1)
-			a[i]=1;
-			else
-				a[i]=0;
-		for(i=3;i<=mare/3;i+=2)
-			if(a[i]==1)
-				for(k=i*3;k<=mare;k+=i*2)
-					a[k]=0;
-		
-		
-		
-		
+	
 		int u,nr=8,total=13,n=49,j=8;
-		i=7;
-		//System.out.println(nr+" "+total+" :"+n+"+ "+(n+j)+" +"+(n+3*j));
-		while(nr*10>total)
+		//System.out.println(nr+" "+total+" :"+" "+(n+j)+" "+(n+2*j)+" "+(n+3*j)+" "+(n+4*j)+" ");		
+		while(nr*10>=total)
 		{
 			for(u=0;u<=3;u++)
 			{
 				n+=j;
-				nr+=a[n];
+		
+				nr+=prim(n);
 			}
 			total+=4;
 			j+=2;
-			i++;
-			//System.out.println(nr+" "+total+" :"+n+" +"+(n+j)+" +"+(n+3*j));
+			
+	//System.out.println(nr+" "+total+" :"+" "+(n+j)+" "+(n+2*j)+" "+(n+3*j)+" "+(n+4*j)+" ");
 		}
-		System.out.println(j-1);
+		System.out.println(j-1);/*
+		nr=1;
+		int i=3;
+		while(nr!=200000)
+		{
+			nr+=prim(i);
+			i+=2;
+		}
+		System.out.println(i-2);*/
+	
+	}
+	public static int prim(int x)
+	{
+		int i,y=minim(x);
+		if(x%2==0)
+			return 0;
+		for(i=3;i<y;i+=2)
+			if(x%i==0)
+				return 0;
+		return 1;
+	}
+	public static int minim(int x)
+	{
+		if(x<36000)
+			return x;
+		else
+			return 36000;
 	}
 	
 }	
