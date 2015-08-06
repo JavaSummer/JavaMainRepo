51cdd35d0f0ca3af0bbac7bb9d8eeb88024562ef
mariahada <mhada02@gmail.com>
2015-08-06 22:53:52 +0300

    Assignment 3 update.

--- Students/Hada Maria/Assignment 3/src/javasmmr/zoowsome/controllers/MainController.java
+++ Students/Hada Maria/Assignment 3/src/javasmmr/zoowsome/controllers/MainController.java
@@ -1,6 +1,7 @@
 package javasmmr.zoowsome.controllers;
 
 import javasmmr.zoowsome.models.animals.Animal;
+import javasmmr.zoowsome.models.animals.*;
 import javasmmr.zoowsome.services.factories.AnimalFactory;
 import javasmmr.zoowsome.services.factories.Constants;
 import javasmmr.zoowsome.services.factories.SpeciesFactory;
@@ -10,16 +11,35 @@ public class MainController {
 	public static void main(String[] args) throws Exception {
 
 		AnimalFactory abstractFactory = new AnimalFactory();
-
 		SpeciesFactory speciesFactory1 = abstractFactory.getSpeciesFactory(Constants.Species.Mammals);
-		Animal a1 = speciesFactory1.getAnimal(Constants.Animals.Mammals.Monkey);
-		System.out.printf("We have an animal with %d legs!\n", a1.getNrOfLegs());
-		System.out.println("It's name is " + a1.getName() + "!");
-		
-		SpeciesFactory speciesFactory2 = abstractFactory.getSpeciesFactory(Constants.Species.Aquatics);
-		Animal a2 = speciesFactory2.getAnimal(Constants.Animals.Aquatics.Swordfish);
-		System.out.printf("We have an animal with %d legs!\n", a2.getNrOfLegs());
-		System.out.println("It's name is " + a2.getName() + "!");
+		Animal a = speciesFactory1.getAnimal(Constants.Animals.Mammals.Monkey);
+		System.out.printf("We have an animal with %d legs!\n", a.getNrOfLegs());
+		System.out.println("Its name is " + a.getName() + "!\n");
+
+		// Twist 1
+		String[] species = { Constants.Species.Aquatics, Constants.Species.Mammals, Constants.Species.Birds,
+				Constants.Species.Insects, Constants.Species.Reptiles };
+
+		String[][] animals = {
+				{ Constants.Animals.Aquatics.Dolphin, Constants.Animals.Aquatics.Jellyfish,
+						Constants.Animals.Aquatics.Swordfish },
+				{ Constants.Animals.Mammals.Cow, Constants.Animals.Mammals.Tiger, Constants.Animals.Mammals.Monkey },
+				{ Constants.Animals.Birds.Flamingo, Constants.Animals.Birds.Hummingbird, Constants.Animals.Birds.Owl },
+				{ Constants.Animals.Insects.Bee, Constants.Animals.Insects.Beetle,
+						Constants.Animals.Insects.Butterfly },
+				{ Constants.Animals.Reptiles.Turtle, Constants.Animals.Reptiles.Iguana,
+						Constants.Animals.Reptiles.Viper } };
+
+		int i = 0;
+		do {
+			int iRandomAnimal = (int) (Math.random() * 3);
+			int iRandomSpecies = (int) (Math.random() * 5);
+			SpeciesFactory sf = abstractFactory.getSpeciesFactory(species[iRandomSpecies]);
+			Animal randomAnimal = sf.getAnimal(animals[iRandomSpecies][iRandomAnimal]);
+			System.out.printf("We have an animal with %d legs!\n", randomAnimal.getNrOfLegs());
+			System.out.println("Its name is " + randomAnimal.getName() + "!");
+			i++;
+		} while (i >= 50);
 	}
 
 }
