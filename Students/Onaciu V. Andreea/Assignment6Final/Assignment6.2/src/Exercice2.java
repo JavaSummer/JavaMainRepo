3af30dcba22549a107df4cd5df9f35d9ddc0783f
andreeaonaciu <andreeaonaciu@yahoo.com>
2015-09-12 15:31:10 +0300

    ASSIGNMENT 6.2

---
+++ Students/Onaciu V. Andreea/Assignment6Final/Assignment6.2/src/Exercice2.java
@@ -0,0 +1,17 @@
+
+public class Exercice2 {
+	      /************EXERCICE2**********
+	      2. What about code that is analogous to that in Question 1, but that uses ArrayList? For example:
+          ArrayList<Point> a = new ArrayList<Point>();
+          ArrayList<Object> b;
+          b = a;
+          b.add(new Point(10,20));
+          
+          **************ANSWER***************
+       
+            The code doesn't compile, it has an error at the assignment b=a (ArrayList<Point> cannot be 
+          converted into ArrayList<Object>.
+	 
+	      *****************************/
+
+}
