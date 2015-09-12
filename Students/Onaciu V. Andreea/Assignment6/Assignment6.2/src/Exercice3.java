bdb2d78fbf49e126e5ba1f7668e9880ca0ce1b49
andreeaonaciu <andreeaonaciu@yahoo.com>
2015-09-12 13:10:18 +0300

    Assignment6.2

---
+++ Students/Onaciu V. Andreea/Assignment6/Assignment6.2/src/Exercice3.java
@@ -0,0 +1,33 @@
+
+public class Exercice3 {
+/***************EXERCICE3*************
+    
+     3. Sketch the class definition and method signatures for a Stack class, parameterized by the type of element on the
+    stack. Give the method signatures for push, pop, and isEmpty.
+ 
+ ****************ANSWER***************
+  class Stack(){
+      //Class short definition: The Stack class represents a last in first out stack of objects. 
+     **It extends the Vector class with 5 operations that allow
+       a vector to be treated as Stack.
+      
+     //PUSH METHOD:
+       T push(T element){
+       // pushes an element onto the top of the stack and returns the element argument.
+      } 
+      
+     //POP METHOD:
+      T pop(){
+        // it removes the object at the top of the stack and returns that object.
+        * If the stack happens to be empty,the method will throw the EmptyStackException.
+      }
+      
+     //ISEMPTY METHOD:
+      boolean isEmpty(){
+      // This method returns true if the stack has no elements in it, or false if it has at least one element.
+      }
+      
+      }
+     
+ *************************************/
+}
